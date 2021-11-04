e9769af14679034b1987d48388274a85dff77c17
Håkan Alexander <hakan.alexander@gmail.com>
2021-11-04 20:24:23 +0100

    Add Auth0

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,14 +4,21 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
+    <Auth0Provider
+domain="dev-i4khz7eb.eu.auth0.com"
+clientId="elJ0xoXLQraN7PvdFQ1Kojpc9LEj8qEc"
+redirectUri={window.location.origin}
+    >
   <BrowserRouter basename={baseUrl}>
     <App />
-  </BrowserRouter>,
+        </BrowserRouter>
+    </Auth0Provider>,
   rootElement);
 
 registerServiceWorker();
